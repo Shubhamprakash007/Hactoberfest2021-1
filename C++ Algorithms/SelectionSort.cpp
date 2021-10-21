0ed3f2c2475b382ff71fdf704edeed38cf07e0bf
Shubham Prakash <89865676+Shubhamprakash007@users.noreply.github.com>
2021-10-21 12:36:55 +0530

    Update SelectionSort.cpp
    Selection sort using function

--- C++ Algorithms/SelectionSort.cpp
+++ C++ Algorithms/SelectionSort.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int a[5] = {30, 17, 20, 3, 48};
-    int i, key, j , temp;
-    for (i = 0; i < 5; i++)      //outer loop for swapping of first element and iterative element
-    {
-        for (j = i+1; j < 5; j++)  // inner loop to check elements and swap
-        {
-            if (a[i] > a[j])
-            {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-
-    }
-for(i=0;i<5;i++){
-    cout << a[i] << " ";
+#include<stdio.h>
+int selectionsort(int a[],int n){
+	for(int i=0;i<n-1;i++){
+	int min=i;
+		for(int j=i+1;j<n;j++){
+			if(a[min]>a[j]){
+				min=j;
+			}	
+		}
+		int temp=a[i];
+		a[i]=a[min];
+		a[min]=temp;
+	}
 }
-    
+
+
+int main(){
+	int a[20],n;
+	printf("enter size you want for array :");
+	scanf("%d",&n);
+	
+	printf(" enter elemets of array :");
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+	selectionsort(a,n);
+    printf("sorted array :");
+	for(int i=0;i<n;i++){
+		printf("%d\t",a[i]);
+	}
 }
